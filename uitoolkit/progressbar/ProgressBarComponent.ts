b6e546a5c20655f0766be9f3b453f29620232875
Csutorás Ármin <csutorasa@gmail.com>
2017-03-19 14:43:31 +0100

    ProgressBar fix

--- uitoolkit/progressbar/ProgressBarComponent.ts
+++ uitoolkit/progressbar/ProgressBarComponent.ts
@@ -12,30 +12,25 @@ export class ProgressBarComponent implements AfterViewInit {
 	@Input('max') max: number;
 	@ViewChild('fill') div: ElementRef;
 
-	private tempValue: number;
+	private innerValue: number;
 
 	@Input('value')
 	public set value(value: number) {
-		if(this.div) {
+		if (this.div) {
 			this.div.nativeElement.style.width = this.calculatePercent(value) + '%';
-		} else {
-			this.tempValue = (value - this.min) / this.max - this.min;
 		}
+		this.innerValue = value;
 	}
 
 	ngAfterViewInit() {
-		if(this.tempValue) {
-			this.value = this.tempValue;
-		} else {
-			this.value = this.value || this.min;
-		}
+		this.value = this.innerValue;
 	}
 
 	protected calculatePercent(value: number): number {
 		const calculated = ((value - this.min) / this.max - this.min) * 100;
-		if(calculated > 100)
+		if (calculated > 100)
 			return 100;
-		else if(calculated < 0)
+		else if (calculated < 0)
 			return 0;
 		else
 			return calculated;

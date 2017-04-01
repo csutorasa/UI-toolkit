92a91db02d1fb29309572c0510c53557186a9539
Csutorás Ármin <csutorasa@gmail.com>
2017-04-01 15:39:29 +0200

    Searchbox UX improvements

--- showcase/src/modules/searchbox.ts
+++ showcase/src/modules/searchbox.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnDestroy } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
 	selector: 'searchbox-test',
@@ -6,7 +7,7 @@ import { Component, Input, OnDestroy } from '@angular/core';
 	<h3>Plain</h3>
     <uisearchbox [dataSource]="dataSource">
 		<uiinputtemplate>
-			<input template="let data=data;let events=events" [ngModel]="data.value" (ngModelChange)="events.onChange($event)" (keyup)="events.onKeyup($event)"/>
+			<input template="let data=data;let events=events" [ngModel]="data.value" (ngModelChange)="events.onChange($event)" (keydown)="events.onKeydown($event)"/>
 		</uiinputtemplate>
 		<uilistelement>
 			<div template="let value=value;" class="list-element-nostyle">{{value}}</div>
@@ -15,7 +16,20 @@ import { Component, Input, OnDestroy } from '@angular/core';
 	<h3>With design</h3>
     <uisearchbox [dataSource]="dataSource" value="test">
 		<uiinputtemplate>
-			<input template="let data=data;let events=events" class="input-template" placeholder="Test input" [ngModel]="data.value" (ngModelChange)="events.onChange($event)" (keyup)="events.onKeyup($event)" spellcheck="false"/>
+			<input template="let data=data;let events=events" class="input-template" placeholder="Test input" [ngModel]="data.value" (ngModelChange)="events.onChange($event)" (keydown)="events.onKeydown($event)" spellcheck="false"/>
+		</uiinputtemplate>
+		<uilistelement>
+			<div template="let value=value;let selected=selected;let first=first;let last=last;" class="list-element"
+			[ngClass]="{'list-element-selected': selected, 'list-element-first': first, 'list-element-last': last}">{{value}}</div>
+		</uilistelement>
+		<uilistelementseparator>
+			<div template class="list-element-separator"></div>
+		</uilistelementseparator>
+	</uisearchbox>
+	<h3>With remote search</h3>
+    <uisearchbox [dataSource]="remoteDataSource" value="test">
+		<uiinputtemplate>
+			<input template="let data=data;let events=events" class="input-template" placeholder="Test input" [ngModel]="data.value" (ngModelChange)="events.onChange($event)" (keydown)="events.onKeydown($event)" spellcheck="false"/>
 		</uiinputtemplate>
 		<uilistelement>
 			<div template="let value=value;let selected=selected;let first=first;let last=last;" class="list-element"
@@ -28,7 +42,9 @@ import { Component, Input, OnDestroy } from '@angular/core';
 })
 export class SearchBoxTestComponent {
 	dataSource: (text:string) => Promise<string[]>;
-	constructor() {
+	remoteDataSource: (text:string) => Promise<string[]>;
+	constructor(http: Http) {
 		this.dataSource = (text:string) => Promise.resolve([text + '1', text + '2', text + text + '3']);
+		this.remoteDataSource = (text:string) => http.post('/search', text).map(res => res.json()).toPromise();
 	}
 }
--- uitoolkit/searchbox/SearchboxComponent.ts
+++ uitoolkit/searchbox/SearchboxComponent.ts
@@ -8,7 +8,7 @@ export interface InputTemplateData {
 	data: { value: string };
 	events: {
 		onChange: (value: string) => void,
-		onKeyup: (event: KeyboardEvent) => void
+		onKeydown: (event: KeyboardEvent) => void
 	};
 }
 export interface ElementDataInput { 
@@ -33,8 +33,8 @@ export class ElementData {
 	template: `
 	<div (focusout)="lostFocus($event)">
 		<template [templatecreator]="inputTemplate.template" [data]="inputTemplateData"></template>
-		<div class="searchbox-element-list" tabindex="0" #searchBoxList (keyup)="keyup($event)">
-			<div [hidden]="!isOpen" class="searchbox-list-elements">
+		<div class="searchbox-element-list" tabindex="0" #searchBoxList (keydown)="keydown($event)">
+			<div [hidden]="!isOpen" class="searchbox-list-elements" #elementContainer>
 				<div *ngFor="let d of dataSource;let i=index;let last=last;" (click)="selectValue(d.value)" class="searchbox-list-element">
 					<template [templatecreator]="listElement.template" [data]="d"></template>
 					<template [templatecreator]="listElementSeparator.template" *ngIf="!last && listElementSeparator"></template>
@@ -48,10 +48,11 @@ export class SearchboxComponent implements AfterContentInit {
 	@ContentChild(ListElement) public listElement: ListElement;
 	@ContentChild(ListElementSeparator) public listElementSeparator: ListElementSeparator;
 	@ViewChild('searchBoxList') public searchBoxList: ElementRef;
+	@ViewChild('elementContainer') public elementContainer: ElementRef;
 	@Input('dataSource') dataSourceFactory: (text: string) => Promise<string[]>;
 	@Output('valueChange') valueChange: EventEmitter<string> = new EventEmitter<string>();
 
-	inputTemplateData: InputTemplateData = { data: { value: '' }, events: { onChange: (value) => this.onChange(value), onKeyup: (event) => this.keyup(event) } };
+	inputTemplateData: InputTemplateData = { data: { value: '' }, events: { onChange: (value) => this.onChange(value), onKeydown: (event) => this.keydown(event) } };
 	dataSource: ElementData[] = [];
 	isOpen: boolean = false;
 	elementDataInput: ElementDataInput = {
@@ -90,18 +91,32 @@ export class SearchboxComponent implements AfterContentInit {
 		}
 	}
 
-	keyup(event: KeyboardEvent): void {
+	keydown(event: KeyboardEvent): void {
 		if (!this.isOpen) {
 			return;
 		}
 		if (event.keyCode === 13) { // Enter
 			this.selectValue(this.dataSource[this.elementDataInput.selectedIndex].value);
 		} else if (event.keyCode === 38) { // Up
-			if (this.elementDataInput.selectedIndex > 0)
+			if (this.elementDataInput.selectedIndex > 0) {
 				this.elementDataInput.selectedIndex--;
+				const container = <HTMLScriptElement>this.elementContainer.nativeElement;
+				const div = container.children[this.elementDataInput.selectedIndex];
+				if((<any>div).offsetTop < container.scrollTop) {
+					container.scrollTop -= container.scrollTop - (<any>div).offsetTop;
+				}
+			}
 		} else if (event.keyCode === 40) { // Down
-			if (this.elementDataInput.selectedIndex + 1 < this.dataSource.length)
+			if (this.elementDataInput.selectedIndex + 1 < this.dataSource.length) {
 				this.elementDataInput.selectedIndex++;
+				const container = <HTMLScriptElement>this.elementContainer.nativeElement;
+				const div = container.children[this.elementDataInput.selectedIndex];
+				const offsetBottom = (<any>div).offsetTop + div.getBoundingClientRect().height;
+				const scrollBottom = container.scrollTop + container.getBoundingClientRect().height;
+				if(offsetBottom > scrollBottom) {
+					container.scrollTop += offsetBottom - scrollBottom;
+				}
+			}
 		}
 	}
 
@@ -116,6 +131,8 @@ export class SearchboxComponent implements AfterContentInit {
 			this.dataSource = items.map((item, index) => { return new ElementData(this.elementDataInput, item, index === items.length -1, index); });
 			this.isOpen = true;
 			this.elementDataInput.selectedIndex = 0;
+			const container = <HTMLScriptElement>this.elementContainer.nativeElement;
+			container.scrollTop = 0;
 		});
 	}
 }

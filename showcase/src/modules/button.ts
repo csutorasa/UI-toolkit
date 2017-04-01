fd7bd875930cd47fe8786230ca634e392105705f
Csutorás Ármin <csutorasa@gmail.com>
2017-04-01 13:45:59 +0200

    Button implementation

--- showcase/src/modules/button.ts
+++ showcase/src/modules/button.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'button-test',
 	template: `
-    <button>button1</button>
-    <button>button2</button>
-    <button>button3</button>
-    <button>button4</button>`,
+    <button>Basic</button>
+    <button buttonstyle="default">Default</button>
+    <button buttonstyle="primary">Primary</button>
+    <button buttonstyle="success">Success</button>
+    <button buttonstyle="info">Info</button>
+    <button buttonstyle="warning">Warning</button>
+    <button buttonstyle="danger">Danger</button>
+    <button buttonstyle="link">Link</button>`,
 })
 export class ButtonTestComponent {
     
--- uitoolkit/button/ButtonDirective.ts
+++ uitoolkit/button/ButtonDirective.ts
@@ -1,10 +1,44 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
 	selector: 'button',
 })
 export class ButtonDirective {
-	constructor() {
-		console.log('Button found');
+
+	static BOOTSTRAP_STYLES = [
+		'default',
+		'primary',
+		'success',
+		'info',
+		'warning',
+		'danger',
+		'link'
+	]
+
+	constructor(protected element: ElementRef) {
+		const classList = this.getClassList();
+		if(!classList.contains('btn')) {
+			classList.add('btn');
+		}
+	}
+
+	@Input('buttonstyle')
+	public set style(style: string) {
+		if(style && !ButtonDirective.BOOTSTRAP_STYLES.some(s => s === style)) {
+			throw 'Invalid Button Style!';
+		}
+		const classList = this.getClassList();
+		ButtonDirective.BOOTSTRAP_STYLES.forEach(s => {
+			if(classList.contains('btn-' + s)) {
+				classList.remove('btn-' + s);
+			}
+		});
+		if(style) {
+			classList.add('btn-' + style);
+		}
+	}
+
+	protected getClassList(): DOMTokenList {
+		return (<HTMLScriptElement>this.element.nativeElement).classList;
 	}
 }

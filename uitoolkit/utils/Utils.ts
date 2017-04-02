a6ac0a8e7396930cdd2db3f6800f278f3c6af642
Csutorás Ármin <csutorasa@gmail.com>
2017-04-02 22:31:07 +0200

    Utils sort added

--- uitoolkit/utils/Utils.ts
+++ uitoolkit/utils/Utils.ts
@@ -18,4 +18,18 @@ export class Utils {
         });
         return promise;
     }
+
+    public static DefaultSort<T>(a: T, b: T, selector?: (e: T) => any): number {
+        const aVal = selector ? selector(a) : a;
+        const bVal = selector ? selector(b) : b;
+        if (aVal == null)
+            return -1;
+        if (bVal == null)
+            return 1;
+        if (aVal > bVal)
+            return 1;
+        if (aVal < bVal)
+            return -1;
+        return 0;
+    }
 }

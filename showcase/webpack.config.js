d069768f0173b0753a3b337ec6dd3b42165dc44f
Csutorás Ármin <csutorasa@gmail.com>
2017-04-10 23:53:16 +0200

    Refactor

--- showcase/webpack.config.js
+++ showcase/webpack.config.js
@@ -3,24 +3,27 @@ const fs = require('fs');
 const webpack = require('webpack');
 
 const alias = {};
-const filename = path.join('wizyx', 'bundles', 'wizyx.umd.min.js');
+//const filename = path.join('wizyx', 'bundles', 'wizyx.umd.js');
+const filename = path.join('wizyx', 'index.ts');
 try {
-    const innerDevFilename = path.join(__dirname, filename);
+    const innerDevFilename = path.join(__dirname, '..', filename);
     fs.statSync(innerDevFilename);
     alias['wizyx'] = innerDevFilename;
+    console.log('Using workspace wizyx libary!');
 }
 catch(ex) {
     // file not found
     // package will be used if found
     alias['wizyx'] = filename;
+    console.log('Using node_modules wizyx libary!');
 }
 
 module.exports = {
     entry: {
-        'showcase': path.join(__dirname, 'showcase', 'src', './main.ts')
+        'showcase': path.join(__dirname, 'src', './main.ts')
     },
     output: {
-        path: path.join(__dirname, 'showcase', 'bundles'),
+        path: path.join(__dirname, 'bundles'),
         filename: '[name].js'
     },
     resolve: {

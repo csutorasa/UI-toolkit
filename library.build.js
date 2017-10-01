a30c3e3cc73fa4d3a766f6b6f39599761da9142b
Ármin Csutorás <csutorasa@gmail.com>
2017-10-01 23:55:35 +0200

    Better logging for library building

--- library.build.js
+++ library.build.js
@@ -2,6 +2,9 @@ const child_process = require('child_process');
 const path = require('path');
 
 if(process.env.BUILD_LIBRARY === 1 || process.env.BUILD_LIBRARY === 'true') {
-    console.log('Building workspace version of library.')
-    child_process.execSync('npm install', { cwd: path.join(__dirname, 'wizyx') });
+    const directory = path.join(__dirname, 'wizyx');
+    const command = 'npm install';
+    console.log('Building workspace version of library.');
+    console.log(directory + '/' + command);
+    child_process.execSync(command, { cwd: directory });
 }

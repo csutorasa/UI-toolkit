21d07f3a7c047927251c1cba98aba0833cd7b782
Ármin Csutorás <csutorasa@gmail.com>
2017-10-03 00:20:13 +0200

    Build library fix

--- library.build.js
+++ library.build.js
@@ -2,9 +2,12 @@ const child_process = require('child_process');
 const path = require('path');
 
 if(process.env.BUILD_LIBRARY === 1 || process.env.BUILD_LIBRARY === 'true') {
-    const directory = path.join(__dirname, 'wizyx');
-    const command = 'npm install';
     console.log('Building workspace version of library.');
+    const directory = path.join(__dirname, 'wizyx');
+    let command = 'npm install';
+    console.log(directory + '/' + command);
+    child_process.execSync(command, { cwd: directory });
+    let command = 'npm run rebuild';
     console.log(directory + '/' + command);
     child_process.execSync(command, { cwd: directory });
 }

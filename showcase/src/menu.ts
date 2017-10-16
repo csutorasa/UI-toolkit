c5851c26d5bdccca3a7459029f7fccfd164ecff7
Ármin Csutorás <csutorasa@gmail.com>
2017-10-16 23:12:58 +0200

    Reworked menu structure

--- showcase/src/menu.ts
+++ showcase/src/menu.ts
@@ -5,18 +5,26 @@ import { ProgressBarTestComponent } from './modules/progressbar';
 
 @Component({
     selector: 'mainmenu',
-    template: `<ul>
-    <li><a class="mainmenu-item" routerLink="/localize">{{"localize" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/progressbar">{{"progressbar" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/slider">{{"slider" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/draganddrop">{{"draganddrop" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/button">{{"button" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/expander">{{"expander" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/searchbox">{{"searchbox" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/fileupload">{{"fileuploader" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/datagrid">{{"datagrid" | localize}}</a></li>
-    <li><a class="mainmenu-item" routerLink="/theme">{{"theme" | localize}}</a></li>
-</ul>`,
+    template: `<div>
+    <span class="showcase-subheader">Components</span>
+    <ul>
+        <li><a class="mainmenu-item" routerLink="/button">{{"button" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/datagrid">{{"datagrid" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/expander">{{"expander" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/fileupload">{{"fileuploader" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/progressbar">{{"progressbar" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/searchbox">{{"searchbox" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/slider">{{"slider" | localize}}</a></li>
+    </ul>
+</div>
+<div>
+    <span class="showcase-subheader">Services</span>
+    <ul>
+        <li><a class="mainmenu-item" routerLink="/draganddrop">{{"draganddrop" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/localize">{{"localize" | localize}}</a></li>
+        <li><a class="mainmenu-item" routerLink="/theme">{{"theme" | localize}}</a></li>
+    </ul>
+</div>`,
 })
 export class MainMenuComponent {
 }
